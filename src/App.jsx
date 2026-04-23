9384e9dcb0eeeb9c14e7452fa0e829bd73faac2d
subbujod <subrahmanyamnarlakanti@gmail.com>
2026-04-23 20:28:03 +0530

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-blue-600 underline text-center mt-10">
+  Hello MERN Estate!
+</h1>
+    </div>
+  )
+}
