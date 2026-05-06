e0e4bc77832651bc48c118d25a2c7fb82605daa5
subbujod <subrahmanyamnarlakanti@gmail.com>
2026-05-06 17:42:15 +0530

    add redux-persist

--- mern-estate/my-project/src/redux/store.js
+++ mern-estate/my-project/src/redux/store.js
@@ -1,9 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// --- BULLETPROOF VITE STORAGE FIX ---
+// We bypass the buggy redux-persist import completely
+// and tell it exactly how to use the browser's local storage.
+const customStorage = {
+  getItem: (key) => {
+    return Promise.resolve(window.localStorage.getItem(key));
+  },
+  setItem: (key, value) => {
+    window.localStorage.setItem(key, value);
+    return Promise.resolve(value);
+  },
+  removeItem: (key) => {
+    window.localStorage.removeItem(key);
+    return Promise.resolve();
+  },
+};
+// ------------------------------------
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: 'root',
+  storage: customStorage, // <-- We use the custom wrapper here
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userReducer },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false,
-  }),   
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Required for Redux Persist
+    }),
+});
+
+export const persistor = persistStore(store);
